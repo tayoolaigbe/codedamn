e3461d78a2820cac4525aac4564d1b186c61179b
tayoolaigbe <tayoolaigbe@outlook.com>
2022-12-07 08:38:50 +0000

    Understanding the useState hook

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 
+const randomQuotes = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQR'];
 function App() {
 	const [counter, setCounter] = useState(0);
+	const [quote, setQuote] = useState(randomQuotes[0]);
 
 	function increment() {
 		setCounter(count => count + 1);
@@ -10,13 +12,21 @@ function App() {
 		setCounter(count => count - 1);
 	}
 
+	function randomise() {
+		const randomQuote =
+			randomQuotes[Math.floor(Math.random() * randomQuotes.length)];
+		setQuote(randomQuote);
+	}
 	return (
 		<div className="App">
 			<h1>
 				Counter: <span>{counter}</span>
+				<div>---------</div>
+				<div>{quote}</div>
 			</h1>
 			<button onClick={increment}>Increment</button>
 			<button onClick={decrement}>Decrement</button>
+			<button onClick={randomise}>Randomise</button>
 		</div>
 	);
 }
