87f876fa588a952bd889e75fb295ec7775244b51
Eyitayo Olaigbe <tayoolaigbe@outlook.com>
2022-12-10 23:42:33 +0000

    Adding modifications

--- scrimba/index.js
+++ scrimba/index.js
@@ -18,14 +18,17 @@ Create a navbar in JSX:
 */
 
 const navbar = (
-  <nav>
-      <h1>Bob's Bistro</h1>
-      <ul>
-          <li>Menu</li>
-          <li>About</li>
-          <li>Contact</li>
-      </ul>
-  </nav>
+  <div>
+        <img src="./react-logo.png" width="40px" />
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
 )
 
 ReactDOM.render(navbar, document.getElementById("root"))
