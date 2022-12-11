8c839d11a9bb54fc82a1aa75e5fb5a56c02e34eb
Eyitayo Olaigbe <tayoolaigbe@outlook.com>
2022-12-11 17:22:22 +0000

    Organising files

--- scrimba/index.js
+++ scrimba/index.js
@@ -1,62 +1,17 @@
-// const page = (
-//     <div>
-//         <h1 className="header">This is JSX</h1>
-//         <p>This is a paragraph</p>
-//     </div>
-// )
+// import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Header';
+import Footer from './Footer';
+import MainContent from './MainContent';
 
-/* 
-Challenge: 
-
-Create a navbar in JSX:
-    - Use the semantic `nav` element as the parent wrapper
-    - Have an h1 element with the brand name of your "website"
-    - Insert an unordered list for the other nav elements
-        - Inside the `ul`, have three `li`s for "Pricing",
-        "About", and "Contact"
-    - Don't worry about styling yet - it'll just be plain-looking HTML for now
-*/
-
-function Header() {
-	return (
-		<header>
-			<nav>
-				<img src="./react-logo.png" width="40px" />
-			</nav>
-		</header>
-	);
-}
-
-function Footer() {
-	return (
-		<footer>
-			<small>© 2021 gidi development. All rights reserved.</small>
-		</footer>
-	);
-}
-
-function Main() {
-	return (
-		<div>
-			<h1>Fun facts about React</h1>
-			<ul>
-				<li>Was first released in 2013</li>
-				<li>Was originally created by Jordan Walke</li>
-				<li>Has well over 100K stars on GitHub</li>
-				<li>Is maintained by Facebook</li>
-				<li>Powers thousands of enterprise apps, including mobile apps</li>
-			</ul>
-		</div>
-	);
-}
-function Page() {
+function App() {
 	return (
 		<div>
 			<Header />
-			<Main />
+			<MainContent />
 			<Footer />
 		</div>
 	);
 }
 
-ReactDOM.render(<Page />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
