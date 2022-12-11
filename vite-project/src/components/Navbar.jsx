e522863a231be7750488186878870e221c48a22a
Eyitayo Olaigbe <tayoolaigbe@outlook.com>
2022-12-11 19:36:17 +0000

    Finishing the Navbar setup

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 function Navbar() {
-	return <h1>Navbar goes here.</h1>;
+	return (
+		<nav>
+			<img src="./react-icon-small.png" className="nav--icon" />
+			<h3 className="nav--logo_text">React Facts</h3>
+			<h4 className="nav--title">React Course - Project 1.</h4>
+		</nav>
+	);
 }
 
 export default Navbar;
