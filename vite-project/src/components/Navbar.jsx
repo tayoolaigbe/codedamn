8f52185615279a9d1c7d111bb888c621f2984939
Eyitayo Olaigbe <tayoolaigbe@outlook.com>
2022-12-14 11:32:27 +0000

    Trying to fix images

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Navbar() {
 	return (
 		<nav>
-			<img src="./react-icon-small.png" className="nav--icon" />
+			<img src="logo.svg" className="nav--icon" />
 			<h3 className="nav--logo_text">React Facts</h3>
 			<h4 className="nav--title">React Course - Project 1.</h4>
 		</nav>
