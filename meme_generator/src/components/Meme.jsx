3fdf6fb44d391be733be90286d2f30531f3c573e
tayoolaigbe <tayoolaigbe@outlook.com>
2022-12-15 23:58:11 +0000

    FInalised the Meme component

--- meme_generator/src/components/Meme.jsx
+++ meme_generator/src/components/Meme.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
 export default function Meme() {
+	const thingsArray = ['Thing 1', 'Thing 2'];
 	return (
 		<main>
-			<form className='form'>
-				<input type="text"  className='form--input' placeholder='Top text'/>
-				<input type="text"  className='form--input' placeholder='Bottom text'/>
-				<button className='form--button'>Get a new meme image 🖼</button>
+			<form className="form">
+				<input type="text" className="form--input" placeholder="Top text" />
+				<input type="text" className="form--input" placeholder="Bottom text" />
+				<button className="form--button">Get a new meme image 🖼</button>
 			</form>
+			<div>
+				<button>Add Item</button>
+				{
+					/* Insert the things here*/
+					thingsArray.map((thing, index) => (
+						<p key={index}>{thing}</p>
+					))
+				}
+			</div>
 		</main>
 	);
 }
