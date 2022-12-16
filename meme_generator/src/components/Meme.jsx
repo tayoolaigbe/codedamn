dca5193235f70adbb5baa059619aca38ae2ba8dd
tayoolaigbe <tayoolaigbe@outlook.com>
2022-12-16 07:44:57 +0000

    Added a memeData.js to mimick calling an API

--- meme_generator/src/components/Meme.jsx
+++ meme_generator/src/components/Meme.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
+import memesData from '../memesData.js';
 
 export default function Meme() {
-	const thingsArray = ['Thing 1', 'Thing 2'];
+	const [memeImage, setMemeImage] = React.useState('');
+	/**
+	 * Challenge: Save the random meme URL in state
+	 * - Below the div.form, add an <img /> and set the
+	 *   src to the new `memeImage` state you created
+	 */
+
+	function getMemeImage() {
+		const memesArray = memesData.data.memes;
+		const randomNumber = Math.floor(Math.random() * memesArray.length);
+		setMemeImage(memesArray[randomNumber].url);
+	}
+
 	return (
 		<main>
-			<form className="form">
-				<input type="text" className="form--input" placeholder="Top text" />
-				<input type="text" className="form--input" placeholder="Bottom text" />
-				<button className="form--button">Get a new meme image 🖼</button>
-			</form>
-			<div>
-				<button>Add Item</button>
-				{
-					/* Insert the things here*/
-					thingsArray.map((thing, index) => (
-						<p key={index}>{thing}</p>
-					))
-				}
+			<div className="form">
+				<input type="text" placeholder="Top text" className="form--input" />
+				<input type="text" placeholder="Bottom text" className="form--input" />
+				<button className="form--button" onClick={getMemeImage}>
+					Get a new meme image 🖼
+				</button>
 			</div>
+			<img src={memeImage} className="meme--image" />
 		</main>
 	);
 }
